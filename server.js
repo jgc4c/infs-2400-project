7809266f42c5e5577440a3670042c16c06d60206
jgc4c <jgc4c@mtmail.mtsu.edu>
2025-04-24 14:57:48 -0500

    added content

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser')
 const app = express();
 const PORT = 3000;
 
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
-    res.sendFile( path.join( __dirname, 'src', 'html', 'home.html' ) );
+    res.sendFile( path.join( __dirname, 'src', 'html', 'overview.html' ) );
+});
+
+app.get('/:page', (req, res) => {
+    const { page } = req.params;
+    res.sendFile( path.join( __dirname, 'src', 'html', `${page}.html` ) );
 });
 
 app.listen(PORT, () => {
